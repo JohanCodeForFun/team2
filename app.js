416845527023867a97978c7226edf641fc0d5b77
Dimitri <dimpirogovp@hotmail.com>
2023-05-12 22:00:11 +0200

    installed express,cors,mongodb, moved app.js, changed start script to new app.js spot, created getAll request in app,db and route in routes/hus, filled in config, tested and working

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const PORT = 3000;
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const hus = require('./routes/hus');
+app.use('/hus', hus);
+
+app.listen(PORT, () => {
+  console.log('HUS API - Lyssnar på port: ' + PORT);
+});
---
+++ routes/hus.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs')
+
+const db = require('../db');
+
+router.get('/all', async(req, res) => {
+  try {
+    /*
+    if(fileExist)
+    */
+    let data = await db.getAll();
+    return res.json(data);
+  } catch (e) {
+    console.error(e);
+    return res.statusCode(500);
+  }
+});
+
+module.exports = router;
