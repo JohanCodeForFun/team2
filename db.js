dc088660829fe4af729d4466f1de1404379de15a
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-05-15 15:25:04 +0200

    created function for adding one static house

--- db.js
+++ db.js
@@ -5,6 +5,7 @@ const connectionStr = config.CONNECTION_STRING;
 const client = new MongoClient(connectionStr);
 const db = client.db('Team2');
 const collection = db.collection('HusTest');
+const House = require('./models/house');
 
 client.connect();
 
@@ -55,7 +56,36 @@ async function update(_id, dataToUpdate) {
 
 }
 
+// lagt till funktion att skapa ett statiskt hus när
+// post request görs till http://localhost:3000/hus/add
+
+// Behöver uppdateras med dynamisk data som kommer med post body.
+
+async function addHouse() {
+  const house = new House({
+    adress: "Strandvägen 12",
+    city: "Stockholm",
+    postalCode: 12345,
+    rooms: 20,
+    floor: 0,
+    price: 40000000,
+    yearBuilt: 2022,
+    size: 260,
+    sold: false,
+    description: "Ditt drömboende på strandvägen!",
+    images: [
+      "http//bild1.jpg",
+      "http//bild2.jpg",
+      "http//bild3.jpg"
+    ],
+  })
+  const result = await collection.insertOne(house);
+
+  return result
+}
+
 module.exports = {
   getAll,
+  addHouse,
   update
 }
