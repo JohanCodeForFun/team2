82ef6caaf2753be41a17e629fb74175ddedf9acf
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-05-18 09:37:03 +0200

    add space

--- helpers/validateDownPayment.js
+++ helpers/validateDownPayment.js
@@ -1,6 +1,6 @@
 function housePriceIsNumber(number) {
   if (Number.isInteger(number)) {
-    return `För ett hus som kostar ${number.toLocaleString()}kr, blir handpeningen ${(number * 0.2).toLocaleString()}kr (20%).`
+    return `För ett hus som kostar ${number.toLocaleString()} kr, blir handpeningen ${(number * 0.2).toLocaleString()} kr (20%).`
   } else {
     throw "Var vänlig försök igen och ange huspriset som ett nummer.";
   }
